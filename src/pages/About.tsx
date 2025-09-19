9453a253174c74c7c7cabd9ee584a0d5afb79be0
yeshwant <yeshwantreddyanugu@gmail.com>
2025-09-19 20:49:10 +0530

    .

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card"; 
 import { Link } from "react-router-dom";
 import { 
   ArrowRight, 
